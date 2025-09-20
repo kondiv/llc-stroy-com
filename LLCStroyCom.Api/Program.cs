c95b9b2d14ce10a543c26f3fa9d7099989471dd7
kondiv <ilyakondrachin77@gmail.com>
2025-09-20 21:35:01 +0300

    configs

--- LLCStroyCom.Api/Program.cs
+++ LLCStroyCom.Api/Program.cs
@@ -100,11 +100,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 using (var scope = app.Services.CreateScope())
 {

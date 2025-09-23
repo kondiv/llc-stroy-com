513d77099b7928c97e60b38420639c7319abca12
kondiv <ilyakondrachin77@gmail.com>
2025-09-23 21:40:57 +0300

    migrations

---
+++ LLCStroyCom.Infrastructure/Migrations/20250923073537_UserNameProperty.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LLCStroyCom.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserNameProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "user",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "user");
+        }
+    }
+}
---
+++ LLCStroyCom.Infrastructure/Migrations/20250923102215_NullCompanyForUser.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LLCStroyCom.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullCompanyForUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_user_company_CompanyId",
+                table: "user");
+
+            migrationBuilder.RenameColumn(
+                name: "CompanyId",
+                table: "user",
+                newName: "company_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_user_CompanyId",
+                table: "user",
+                newName: "IX_user_company_id");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "company_id",
+                table: "user",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_user_company_company_id",
+                table: "user",
+                column: "company_id",
+                principalTable: "company",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_user_company_company_id",
+                table: "user");
+
+            migrationBuilder.RenameColumn(
+                name: "company_id",
+                table: "user",
+                newName: "CompanyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_user_company_id",
+                table: "user",
+                newName: "IX_user_CompanyId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CompanyId",
+                table: "user",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_user_company_CompanyId",
+                table: "user",
+                column: "CompanyId",
+                principalTable: "company",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
